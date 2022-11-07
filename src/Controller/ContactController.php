8cb69fbdd0286e6e644f9d9754ba1430ff99191b
yanis <y.elkajjoui@sudalys.fr>
2022-11-07 14:20:39 +0100

    Changement tout

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -38,7 +38,7 @@ class ContactController extends AbstractController
             return $this->redirectToRoute('contact');
         }
 
-        return $this->render('home/home.html.twig', [
+        return $this->render('contact/contact.html.twig', [
             'form' => $form->createView()
         ]);
     }
